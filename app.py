d545ffbdb4f6c583ed6fd5d7d7180e8e21a57da0
bcentenov5-blip <bcentenov5@gmail.com>
2026-07-03 00:20:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ if db_url and db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql+psycopg2://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     "pool_pre_ping": True,
     "connect_args": {"sslmode": "require"}
